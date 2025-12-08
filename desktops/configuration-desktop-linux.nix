9b4c2383eaaa4ff7f1cab5befe26ba8a217274b7
joshrosso <joshrosso@gmail.com>
2025-12-08 07:32:43 -0700

    mosquitto and unifi

--- desktops/configuration-desktop-linux.nix
+++ desktops/configuration-desktop-linux.nix
@@ -8,6 +8,23 @@
 # TODO(joshrosso): This is all temporary until my server parts arrive
 
 {
+
+  services.unifi.enable = true;
+
+  # https://nixos.wiki/wiki/Mosquitto
+  services.mosquitto = {
+    enable = true;
+    listeners = [
+      {
+        users.emporia-svc-panel = {
+          acl = [ "readwrite #" ];
+          # nix shell nixpkgs#mosquitto --command mosquitto_passwd -c /tmp/passwd root
+          hashedPassword = "$7$101$JdopfXMasddKQLdf$bz8rcvVEKG3I6Y6o+lSUgT44UE5RTBsAGpgEYHE4Sp43ETjOzqWRnxou46dMF3tKnOIefkNN0jjw9taQFOj7dA==";
+        };
+      }
+    ];
+  };
+
   virtualisation.oci-containers = {
     backend = "docker";
     containers.homeassistant = {
