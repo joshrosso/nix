526bc8d8f048447180ccffe5c34c50985e932219
joshrosso <joshrosso@gmail.com>
2025-12-12 12:06:18 -0700

    hermes: color change

--- hermes/specific.nix
+++ hermes/specific.nix
@@ -5,5 +5,9 @@
     if [[ -n "$SSH_CONNECTION" ]]; then
       printf '\e]11;rgb:ca/ca/78\a'
     fi
+
+    printf '\033]11;rgb:40/35/00\007'
+    # Prompt: literal "name @ hermes$ "
+    PS1="$(whoami) @ hermes$ "
   '';
 }
